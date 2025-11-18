47abee56a0c4f7e79986bd798a29b3c103e9088b
rileycochrell <rileycochrell@gmail.com>
2025-11-18 12:25:03 -0700

    Update 3_change_over_years_and_comparison.py

--- pages/3_change_over_years_and_comparison.py
+++ pages/3_change_over_years_and_comparison.py
@@ -168,7 +168,8 @@ def weaponized_arrows_of_truth(metrics, y1_values, y2_values):
             yref="y",
             text=diff_text,
             showarrow=False,
-            font=dict(color="black", size=14),
+            font=dict(color="black", size=12),
+            style=bold
         ))
 
         # Move label slightly to the right of the bar by adjusting x with "xref='x domain'"
